f681986ba13cd82fc49d1e2477af75f74725f108
DillyDaily <ali.finney@gmail.com>
2017-10-26 12:21:41 -0700

    basic pokemon page loading with info from table

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,14 @@
-const index = require("../controllers/index.js");
 const pokemon = require("../controllers/pokemon.js");
 const trainers = require("../controllers/trainers.js");
+var express = require('express');
 module.exports = function(app){
+
+app.get('/', pokemon.index);
+
+app.get('/pokemon', pokemon.getAll);
+
+// app.post('/pokemon', pokemon.create);
+
+// app.get()
 
 }
--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const knex = require("../db/knex.js");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('pokemon/index', {passedInData: "abc"});
-});
+module.exports = {
+  // CHANGE ME TO AN ACTUAL FUNCTION
 
-module.exports = router;
+  index: function(req, res) {
+    res.redirect('/pokemon');
+  },
+  // index: function(req, res) {
+  //   if(!req.session.user){
+  //     req.session.user = [];
+  //   }
+  //   knex('pokemon')
+  //   .then((results)=>{
+  //     res.redner()
+  //   })
+  //   res.redirect('/pokemon');
+  // },
+
+  getAll: function(req, res) {
+    knex('pokemon')
+    .then((result)=>{
+      
+      res.render('pokemon', {pokemon: result})
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+  }
+}
